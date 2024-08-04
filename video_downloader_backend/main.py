2d30b3ad3b623a362d189661fc387555446d1179
Hitesh Kumar <hitesh.kumar61@yahoo.com>
2024-08-04 22:03:12 +0530

    update

--- video_downloader_backend/main.py
+++ video_downloader_backend/main.py
@@ -26,7 +26,7 @@ logger = logging.getLogger(__name__)
 # Enable CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],  # Replace with your frontend URL
+    allow_origins=["http://whale-app-o4uqw.ondigitalocean.app:3000"],  # Replace with your frontend URL
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
